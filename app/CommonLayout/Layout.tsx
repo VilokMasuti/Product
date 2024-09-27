2f76724a65632f5c3aca686867de12b796146cd0
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-09-27 21:49:42 +0530

    Error
    ./app/CommonLayout/Layout.tsx
      Error: Unexpected any. Specify a different type.  @typescript-eslint/no-explicit-any

--- app/CommonLayout/Layout.tsx
+++ app/CommonLayout/Layout.tsx
@@ -4,9 +4,7 @@ import React from 'react'
 import { Suspense } from "react";
 import ReduxProvider from '../provider';
 
-const Layout = async ({ children }: any
-
-) => {
+const Layout = async( { children }: { children: React.ReactNode }) => {
   return (
 <ReduxProvider>
     <Suspense fallback={<div>Loading...</div> }> {children} </Suspense>
